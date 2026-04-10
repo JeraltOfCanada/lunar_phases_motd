a62c2dd0c52b881c5f80ffdd93b31de1f64a4eb6
Jordan M <jordan.moltzan@pm.me>
2026-04-10 16:38:12 -0600

    Changed from dictionary based to class based
    Added all phases into a class system including a check for the current phase, low boundary and high boundary. Implemented a loop at the bottom instead of if/elif/else.

--- lunar_phases.py
+++ lunar_phases.py
@@ -6,17 +6,33 @@ JULIAN_DATE_UNIX_EPOCH = 2440587.5
 JULIAN_DATE_MARCH_NEW_MOON = 2461118.3097222
 LUNAR_PHASE_LENGTH = 29.53
 LUNAR_PHASE_HALFPOINT = 14.765
+NEW_MOON_HIGH = 29.53
+NEW_MOON_LOW = 28.53
+FULL_MOON_HIGH = 14.765
+FULL_MOON_LOW = 13.765
+FIRST_QUARTER_HIGH = 7.3825
+FIRST_QUARTER_LOW = 6.3825
+THIRD_QUARTER_HIGH = 22.1475
+THIRD_QUARTER_LOW = 21.1475
 
 def lunar_phase():
+    """Function calculates where today falls inside a lunar phase."""
     current_julian_date = time.time() / SECONDS_IN_DAY + JULIAN_DATE_UNIX_EPOCH 
     phase_calculation = (current_julian_date - JULIAN_DATE_MARCH_NEW_MOON) % LUNAR_PHASE_LENGTH
-    """Function calculates where today falls inside a lunar phase."""
     return phase_calculation
 
-phase = lunar_phase()
+class LunarPhase:
+    """Class LunarPhase for holding the phase information for each phase and a
+    check at the end if the phase_calculation falls within that class."""
+    def __init__(self, low, high, name, art):
+       self.low = low
+       self.high = high
+       self.name = name
+       self.art = art
+    def is_active(self, phase):
+      return self.low < phase <= self.high
 
-phases_dict = {
-"NEW MOON" : 
+new_moon = LunarPhase(NEW_MOON_LOW, NEW_MOON_HIGH, "NEW MOON", 
 """
        _..._     
      .'     `.    
@@ -24,8 +40,9 @@ phases_dict = {
     :         :  
     `.       .'  
 jgs   `-...-'  
-""", 
-"WAXING CRESCENT" : 
+""")
+
+waxing_crescent = LunarPhase(NEW_MOON_HIGH, FIRST_QUARTER_LOW, "WAXING CRESCENT", 
 """
        _..._     
      .'   `::.    
@@ -33,17 +50,18 @@ jgs   `-...-'
     :       :::  
     `.     .::'  
 jgs   `-..:'' 
-""", 
-"FIRST QUARTER" : 
-"""
+""")
+
+first_quarter = LunarPhase(FIRST_QUARTER_LOW, FIRST_QUARTER_HIGH, "FIRST QUARTER", """
        _..._     
      .'  ::::.    
     :    ::::::    
     :    ::::::  
     `.   :::::'  
 jgs   `-.::''   
-""", 
-"WAXING GIBBOUS" : 
+""")
+
+waxing_gibbous = LunarPhase(FIRST_QUARTER_HIGH, FULL_MOON_LOW, "WAXING GIBBOUS", 
 """ 
        _..._     
      .' .::::.    
@@ -51,8 +69,9 @@ jgs   `-.::''
     :  ::::::::  
     `. '::::::'  
 jgs   `-.::''  
-""", 
-"FULL MOON" : 
+""")
+
+full_moon = LunarPhase(FULL_MOON_LOW, FULL_MOON_HIGH, "FULL MOON", 
 """    
        _..._     
      .:::::::.    
@@ -60,8 +79,9 @@ jgs   `-.::''
     ::::::::::: 
     `:::::::::'  
 jgs   `':::'' 
-""", 
-"WANING GIBBOUS" : 
+""")
+
+waning_gibbous = LunarPhase(FULL_MOON_HIGH, THIRD_QUARTER_LOW, "WANING GIBBOUS", 
 """ 
        _..._     
      .::::. `.    
@@ -69,17 +89,19 @@ jgs   `':::''
     ::::::::  :  
     `::::::' .'  
 jgs   `'::'-'
- """, 
- "THIRD QUARTER" : 
- """
+ """)
+
+third_quarter = LunarPhase(THIRD_QUARTER_LOW, THIRD_QUARTER_HIGH, "THIRD QUARTER", 
+"""
        _..._     
      .::::  `.    
     ::::::    :    
     ::::::    :  
     `:::::   .'  
 jgs   `'::.-'   
-""", 
-"WANING CRESCENT" : 
+""")
+
+waning_crescent = LunarPhase(THIRD_QUARTER_HIGH, NEW_MOON_LOW, "WANING CRESCENT", 
 """  
        _..._     
      .::'   `.    
@@ -87,62 +109,34 @@ jgs   `'::.-'
     :::       :  
     `::.     .'  
 jgs   `':..-'
-"""
-}
-
-# boundary points for lunar phase
-"""
-Waxing crescent: 0-6.375
-First quarter: 6.375-7.375
-Waxing gibbous: 7.375-13.75
-Full moon: 13.75-14.75
-Waning Gibbous: 14.75 - 21.125
-Last quarter: 21.125-22.125
-Waning crescent: 22.125-28.5
-New moon: 28.5-29.5
-"""
-
-# Calculating days to next full/new moon
-cntdwn = math.floor(LUNAR_PHASE_HALFPOINT - phase)
-cntdwn2 = math.floor(LUNAR_PHASE_LENGTH - phase)
-
-# Assigning variable 'day' for propper grammar
-day = "day" if cntdwn == 1 else "days"
-day2 = "day" if cntdwn2 == 1 else "days"
+""")
 
 
-# Remove the # if you want to print the output of the function above
+
+phases = [new_moon, waxing_crescent, first_quarter, waxing_gibbous, full_moon, 
+waning_gibbous, third_quarter, waning_crescent]
+
+# Remove the # if you want to print the output of the lunar_phase() function
 # print(phase)
 
-# Assigning a physical phase to the calculated date 
-# printing ASCII, phase name, and countdown to next new/full moon
-if 0 < phase <= 6.375 :
-    print(phases_dict["WAXING CRESCENT"])
-    print("Today the moon is a WAXING CRESCENT")
-    print(f"{cntdwn} {day} until next Full Moon")
-elif 6.375 < phase <= 7.375 :
-    print(phases_dict["FIRST QUARTER"])
-    print("Today the moon is in its FIRST QUARTER")
-    print(f"{cntdwn} {day} until next Full Moon")
-elif 7.375 < phase <= 13.75 :
-    print(phases_dict["WAXING GIBBOUS"])
-    print("Today the moon is a WAXING GIBBOUS")
-    print(f"{cntdwn} {day} until next Full Moon")
-elif 13.75 < phase <= 14.75 :
-    print(phases_dict["FULL MOON"])
-    print("Today is the FULL MOON!")
-elif 14.75 < phase <= 21.125 :
-    print(phases_dict["WANING GIBBOUS"])
-    print("Today the moon is a WANING GIBBOUS")
-    print(f"{cntdwn2} {day2} until next New Moon")
-elif 21.125 < phase <= 22.125 :
-    print(phases_dict["THIRD QUARTER"])
-    print("Today the moon is in its THIRD QUARTER")
-    print(f"{cntdwn2} {day2} until next New Moon")
-elif 22.125 < phase <= 28.5 :
-    print(phases_dict["WANING CRESCENT"])
-    print("Today the moon is a WANING CRESCENT")
-    print(f"{cntdwn2} {day2} until next New Moon")
-else:
-    print(phases_dict["NEW MOON"])
-    print("Today is the NEW MOON!")
+if __name__ == "__main__":
+
+    phase = lunar_phase()
+   
+    for current_phase in phases:
+        if current_phase.is_active(phase):
+            print(current_phase.art)
+            print(current_phase.name)
+
+# Calculating days to next full/new moon
+    cntdwn = math.floor(LUNAR_PHASE_HALFPOINT - phase)
+    cntdwn2 = math.floor(LUNAR_PHASE_LENGTH - phase)
+
+# Assigning variable 'day' for propper grammar
+    day = "day" if cntdwn == 1 else "days"
+    day2 = "day" if cntdwn2 == 1 else "days"
+
+    if phase < LUNAR_PHASE_HALFPOINT:
+        print(f"{cntdwn} {day} until next Full Moon")
+    else:
+        print(f"{cntdwn2} {day2} until next New Moon")
